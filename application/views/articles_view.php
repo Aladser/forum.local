6fce55e9e2e1ff339e3a085d0a3ca4dff3e856ad
Aladser <aladser@gmail.com>
2023-11-30 07:10:07 +0900

    кнопка добавления темы

--- application/views/articles_view.php
+++ application/views/articles_view.php
@@ -1,20 +1,20 @@
-<div class='w-75 mx-auto'>
+<div class='w-50 mx-auto'>
+    <div id='block-buttons' class='pb-4'>
+        <input type="button" value="Новая тема" class='btn border border-dark fs-5'>
+    </div>
+
     <table class='table-articles table' id='table-articles'>
         <thead>
             <tr> 
-                <th>Тема</th> 
-                <th>Автор</th>
-                <th>Описание</th>
-                <th>Создана</th>  
+                <th class='w-25'>Тема</th> 
+                <th class='w-75 text-center'>Описание</th>
             </tr>
         <thead>
         <tbody>
         <?php foreach ($data['articles'] as $article) { ?>
             <tr class='table-articles__tr' id="<?php echo $article['id']; ?>">
-                <td><?php echo $article['title']; ?></td>
-                <td><?php echo $article['author_id']; ?></td>
-                <td><?php echo $article['summary']; ?></td>
-                <td><?php echo mb_substr($article['time'], 0, 10); ?></td>
+                <td class='w-25'><?php echo $article['title']; ?></td>
+                <td class='w-75 text-center'><?php echo $article['summary']; ?></td>
             </tr>
         <?php }?>
         </tbody>
