ba82435072fc0bada82ab57c9a5fec5168a4c5e7
Aladser <aladser@gmail.com>
2023-11-29 17:29:49 +0900

    обновление верстки

--- application/Controllers/RegController.php
+++ application/Controllers/RegController.php
@@ -10,6 +10,6 @@ class RegController extends Controller
     public function index()
     {
         $data = ['csrfToken' => Controller::createCSRFToken()];
-        $this->view->generate('template_view.php', 'reg_view.php', 'reg.css', 'reg.js', 'Месенджер: регистрация', $data);
+        $this->view->generate('template_view.php', 'reg_view.php', 'reg.css', 'reg.js', 'Деловой чат: регистрация', $data);
     }
 }
