b794715ef63c42a64406de438fc7c24ac262d7dc
Aladser <aladser@gmail.com>
2023-11-30 07:42:01 +0900

    обновлена верстка

--- application/views/articles_view.php
+++ application/views/articles_view.php
@@ -1,6 +1,6 @@
 <div class='w-50 mx-auto'>
     <a href="/article/create">
-        <p class='btn border border-dark p-2 ps-4 pe-4'>Новая тема</p>
+        <p class='d-inline-block border border-dark text-decoration-none text-dark p-3'>Новая тема</p>
     </a>
 
     <table class='table-articles table' id='table-articles'>
--- application/views/create-article_view.php
+++ application/views/create-article_view.php
@@ -1,5 +1,5 @@
 <div class='w-50 mx-auto text-center'>
-    <a href="/article" class=''>
-        <p class='d-inline-block border border-dark text-decoration-none text-dark p-3 '>На главную</p>
+    <a href="/article">
+        <p class='d-inline-block border border-dark text-decoration-none text-dark p-3'>На главную</p>
     </a>
 </div>
