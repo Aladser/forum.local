f0aa3b019b65120a1e0266cf9223ad4b7ab398cb
Aladser <aladser@gmail.com>
2023-12-02 06:50:48 +0900

    обновление дампа БД

--- storage/forum.local create.sql
+++ storage/forum.local create.sql
@@ -10,14 +10,11 @@ create table users
 );
 ALTER TABLE users AUTO_INCREMENT = 1;
 
-insert into users(login, password)
-values ('user1', '$2y$10$0sYGnvEXGZx5wshGxnubUu6oC3/eKUTwXpID2r5aPXVRh8hafbwwG');
-insert into users(login, password)
-values ('user2', '$2y$10$0sYGnvEXGZx5wshGxnubUu6oC3/eKUTwXpID2r5aPXVRh8hafbwwG');
-insert into users(login, password)
-values ('user3', '$2y$10$0sYGnvEXGZx5wshGxnubUu6oC3/eKUTwXpID2r5aPXVRh8hafbwwG');
-insert into users(login, password)
-values ('user4', '$2y$10$0sYGnvEXGZx5wshGxnubUu6oC3/eKUTwXpID2r5aPXVRh8hafbwwG');
+insert into users(login, password) values 
+('user1', '$2y$10$0sYGnvEXGZx5wshGxnubUu6oC3/eKUTwXpID2r5aPXVRh8hafbwwG'),
+('user2', '$2y$10$0sYGnvEXGZx5wshGxnubUu6oC3/eKUTwXpID2r5aPXVRh8hafbwwG'),
+('user3', '$2y$10$0sYGnvEXGZx5wshGxnubUu6oC3/eKUTwXpID2r5aPXVRh8hafbwwG'),
+('user4', '$2y$10$0sYGnvEXGZx5wshGxnubUu6oC3/eKUTwXpID2r5aPXVRh8hafbwwG');
 
 create table articles
 (
@@ -31,16 +28,17 @@ create table articles
 );
 ALTER TABLE articles AUTO_INCREMENT = 1;
 
-INSERT INTO articles (title, author_id, summary, content) values('тема 1', 1, 'Краткое содержание 1', 'Основное содержание 1');
-INSERT INTO articles (title, author_id, summary, content) values('тема 2', 1, 'Краткое содержание 2', 'Основное содержание 2');
-INSERT INTO articles (title, author_id, summary, content) values('тема 3', 1, 'Краткое содержание 3', 'Основное содержание 3');
-INSERT INTO articles (title, author_id, summary, content) values('тема 4', 1, 'Краткое содержание 4', 'Основное содержание 4');
-INSERT INTO articles (title, author_id, summary, content) values('тема 5', 2, 'Краткое содержание 5', 'Основное содержание 5');
-INSERT INTO articles (title, author_id, summary, content) values('тема 6', 2, 'Краткое содержание 6', 'Основное содержание 6');
-INSERT INTO articles (title, author_id, summary, content) values('тема 7', 2, 'Краткое содержание 7', 'Основное содержание 7');
-INSERT INTO articles (title, author_id, summary, content) values('тема 8', 3, 'Краткое содержание 8', 'Основное содержание 8');
-INSERT INTO articles (title, author_id, summary, content) values('тема 9', 3, 'Краткое содержание 9', 'Основное содержание 9');
-INSERT INTO articles (title, author_id, summary, content) values('тема 10', 4, 'Краткое содержание 10', 'Основное содержание 10');
+INSERT INTO articles (title, author_id, summary, content) values
+('тема 1', 1, 'Краткое содержание 1', 'Основное содержание 1'),
+('тема 2', 1, 'Краткое содержание 2', 'Основное содержание 2'),
+('тема 3', 1, 'Краткое содержание 3', 'Основное содержание 3'),
+('тема 4', 1, 'Краткое содержание 4', 'Основное содержание 4'),
+('тема 5', 2, 'Краткое содержание 5', 'Основное содержание 5'),
+('тема 6', 2, 'Краткое содержание 6', 'Основное содержание 6'),
+('тема 7', 2, 'Краткое содержание 7', 'Основное содержание 7'),
+('тема 8', 3, 'Краткое содержание 8', 'Основное содержание 8'),
+('тема 9', 3, 'Краткое содержание 9', 'Основное содержание 9'),
+('тема 10', 4, 'Краткое содержание 10', 'Основное содержание 10');
 
 create table comments
 (
@@ -54,11 +52,12 @@ create table comments
 );
 ALTER TABLE comments AUTO_INCREMENT = 1;
 
-INSERT INTO comments (article_id, author_id, content) values(1, 1, 'Коммент 1');
-INSERT INTO comments (article_id, author_id, content) values(1, 2, 'Коммент 2');
-INSERT INTO comments (article_id, author_id, content) values(1, 3, 'Коммент 3');
-INSERT INTO comments (article_id, author_id, content) values(1, 4, 'Коммент 4');
-INSERT INTO comments (article_id, author_id, content) values(2, 1, 'Коммент 1');
-INSERT INTO comments (article_id, author_id, content) values(2, 1, 'Коммент 2');
-INSERT INTO comments (article_id, author_id, content) values(2, 3, 'Коммент 3');
-INSERT INTO comments (article_id, author_id, content) values(2, 3, 'Коммент 4');
+INSERT INTO comments (article_id, author_id, content) values
+(1, 1, 'Коммент 1'),
+(1, 2, 'Коммент 2'),
+(1, 3, 'Коммент 3'),
+(1, 4, 'Коммент 4'),
+(2, 1, 'Коммент 1'),
+(2, 1, 'Коммент 2'),
+(2, 3, 'Коммент 3'),
+(2, 3, 'Коммент 4');
