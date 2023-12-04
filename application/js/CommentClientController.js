dbb2180a4a27c2895c9907b94727ca1a47354252
Aladser <aladser@gmail.com>
2023-12-04 10:27:59 +0900

    обновлен CommentClientController

--- application/js/CommentClientController.js
+++ application/js/CommentClientController.js
@@ -13,24 +13,26 @@ class CommentClientController {
         // ---данные---
         let formData = new FormData(this.sendCommentForm);
         let timeNow = DBLocalTime.get();
+        // ---обработка ответа от сервера---
+        let process = (data) => {
+            data = JSON.parse(data);
+            if (data.result != 1) {
+                this.msgElement.innerHTML = data;
+            } else {
+                event.target.reset();
+                this.commentList.innerHTML += `
+                    <article class='border-C4C4C4 mb-2'>
+                        <p class='text-start m-0 ps-2 fw-bolder'>${data.comment.author}</p>
+                        <p class='text-start m-0 py-2 ps-3 fs-5'>${data.comment.content}</p>
+                        <p class='text-end m-0 pe-2'>${timeNow}</p>
+                    </article>
+                `;
+            }
+        };
         // ---запрос на сервер---
         ServerRequest.execute(
             this.URL+'/store',
-            (data) => {
-                data = JSON.parse(data);
-                if (data.result != 1) {
-                    this.msgElement.innerHTML = data;
-                } else {
-                    event.target.reset();
-                    this.commentList.innerHTML += `
-                        <article class='border-C4C4C4 mb-2'>
-                            <p class='text-start m-0 ps-2 fw-bolder'>${data.comment.author}</p>
-                            <p class='text-start m-0 py-2 ps-3 fs-5'>${data.comment.content}</p>
-                            <p class='text-end m-0 pe-2'>${timeNow}</p>
-                        </article>
-                    `;
-                }
-            },
+            process,
             "post",
             this.msgElement,
             formData
@@ -39,10 +41,10 @@ class CommentClientController {
 
     // удалить коммент из БД - comment/remove
     remove() {
+        // ---данные---
         let article = document.querySelector(`#${this.activeArticleId}`);
         // действия после успешного удаления данных в БД
         let process = (data) => {
-            console.log(data);
             data = JSON.parse(data);
             if (data.result == 1) {
                 article.remove();
