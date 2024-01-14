8a8907d95783fa0782e5cea273966387c2e2ee2a
Aladser <aladser@gmail.com>
2024-01-15 04:22:05 +0900

    обновление стилей

--- public/views/main_view.php
+++ public/views/main_view.php
@@ -1,8 +1,8 @@
 <div class='container text-center p-2 mt-6'>
     <a href="/login" class='text-decoration-none text-white'>
-        <div class='bg-theme p-2 w-25 mx-auto mt-4 mb-2'> Войти</div>
+        <div class='button bg-theme mx-auto mt-4 mb-2'> Войти</div>
     </a>
     <a href="/register" class='text-decoration-none text-white'>
-        <div class='bg-theme p-2 w-25 mx-auto'>Регистрация</div>
+        <div class='button bg-theme mx-auto'>Регистрация</div>
     </a>
 </div>
