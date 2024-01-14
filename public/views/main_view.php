0dc8b51fb736f89b3e5b73d78102a0fa96edd642
Aladser <aladser@gmail.com>
2024-01-15 07:23:48 +0900

    обновление стилей

--- public/views/main_view.php
+++ public/views/main_view.php
@@ -1,8 +1,8 @@
 <div class='container text-center p-2 mt-6'>
-    <a href="<?php echo $routes['login'];?>" class='text-decoration-none text-white'>
-        <div class='button bg-theme mx-auto mt-4 mb-2'> Войти</div>
+    <a href="<?php echo $routes['login'];?>">
+        <div class='ref button mx-auto mt-4 mb-2'> Войти</div>
     </a>
-    <a href="<?php echo $routes['register'];?>" class='text-decoration-none text-white'>
-        <div class='button bg-theme mx-auto'>Регистрация</div>
+    <a href="<?php echo $routes['register'];?>">
+        <div class='ref button mx-auto'>Регистрация</div>
     </a>
 </div>
