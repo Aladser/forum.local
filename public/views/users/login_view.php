d4340ee1d1e664fa0d805e9344e235caa019ed75
Aladser <aladser@gmail.com>
2024-01-16 12:21:41 +0900

    обновление форм

--- public/views/users/login_view.php
+++ public/views/users/login_view.php
@@ -3,13 +3,21 @@
 
     <form class='form mx-auto' method="POST" action=<?php echo $routes['auth']; ?>>
         <input type="hidden" name='CSRF' value=<?php echo $data['csrf']; ?>>
-        <input type="text" class="form-font theme-border p-3 w-100 mb-3" name='login' class="form__input w-100 mb-2 p-1 border-theme" placeholder="Логин" value="<?php echo $data['user']; ?>" required>
-        <input type="password" class="form-font theme-border p-3 w-100 mb-3" name='password' class="form__input w-100 mb-2 p-1 border-theme" placeholder="Пароль"  required>
-        <input type="submit" class='form-font ref-color border-0 p-3 mb-2 w-100' value="Войти">
-        <a href=<?php echo $routes['home']; ?> class='text-decoration-none'><div class='form-font ref-color mx-auto p-3 w-100'>Назад</div></a>
+
+        <input type="text" name='login' value="<?php echo $data['user']; ?>" 
+        class="form-font theme-border w-100 p-3 mb-3" placeholder="Логин" required>
+
+        <input type="password" name='password' 
+        class="form-font theme-border w-100 p-3 mb-3" placeholder="Пароль" required>
+
+        <input type="submit" value="Войти" class='form-font ref-color border-0 w-100 p-3 mb-2 ' >
+        
+        <a href=<?php echo $routes['home']; ?> class='text-decoration-none'>
+            <div class='form-font ref-color w-100 p-3 mx-auto'>Назад</div>
+        </a>
     </form>
 
     <?php if (isset($data['error'])) {?>
-    <p class='w-50 mx-auto fw-bolder text-danger pt-2 mb-0'><?php echo $data['error']; ?></p>
+    <p class='fw-bolder text-danger w-50 pt-2 mx-auto mb-0'><?php echo $data['error']; ?></p>
     <?php }?>
 </div>
--- public/views/users/register_view.php
+++ public/views/users/register_view.php
@@ -1,24 +1,27 @@
 <div class='container text-center'>
     <h3 class='mt-4 mb-4 text-secondary'>Регистрация нового пользователя</h3>
 
-    <form class='form mx-auto' method="POST" action=<?=$routes['store'];?>>
-        <input type="hidden" name="CSRF" value=<?= $data['csrf']; ?>>
+    <form class='form mx-auto' method="POST" action=<?php echo $routes['store']; ?>>
+        <input type="hidden" name="CSRF" value=<?php echo $data['csrf']; ?>>
 
-        <input type="login" class="form-font theme-border w-100 mb-3 p-2" 
-        id="reg-form__email-input" name='login' value="<?= $data['user']; ?>" placeholder="логин" required>
+        <input type="login" name='login' value="<?php echo $data['user']; ?>" id="reg-form__email-input" 
+        class="form-font theme-border w-100 mb-3 p-2" placeholder="логин" required>
 
+        <input type="password" name='password' id="reg-form__password1-input"
+        class="form-font theme-border w-100 mb-3 p-2" placeholder="пароль (минимум 3 символа)" required>
 
-        <input type="password" class="form-font theme-border w-100 mb-3 p-2" 
-        id="reg-form__password1-input" name='password' placeholder="пароль (минимум 3 символа)" required>
+        <input type="password" name='password_confirm' id="reg-form__password2-input" 
+        class="form-font theme-border w-100 mb-3 p-2" placeholder="подтвердите пароль" required>
 
-        <input type="password" class="form-font theme-border w-100 mb-3 p-2" 
-        id="reg-form__password2-input" name='password_confirm' placeholder="подтвердите пароль" required>
-
-        <input type="submit" class='ref-color form-font border-0 p-3 mb-2 w-100' value="Регистрация" id='reg-form__reg-btn'>
-        <a href=<?=$routes['home'];?> class='text-decoration-none'>
+        <input type="submit" value="Регистрация" id='reg-form__reg-btn' 
+        class='ref-color form-font border-0 p-3 mb-2 w-100'>
+        
+        <a href=<?php echo $routes['home']; ?> class='text-decoration-none'>
             <div class='ref-color form-font mx-auto p-3 w-100'>Назад</div>
         </a>
 
-        <p class='mx-auto fw-bolder text-danger p-2'><?= $data['error']; ?></p>     
+        <?php if (isset($data['error'])) {?>
+        <p class='mx-auto fw-bolder text-danger p-2'><?php echo $data['error']; ?></p> 
+        <?php }?>    
     </form>
 </div>
