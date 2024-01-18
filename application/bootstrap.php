7b1737fe2feab63884a36456e27500bbf6946e08
Aladser <aladser@gmail.com>
2024-01-18 23:14:50 +0900

    update

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -6,4 +6,10 @@ use App\Core\Route;
 
 require __DIR__.'/vendor/autoload.php';
 
+if (!file_exists(dirname(__DIR__, 1).'/logs/access.log')) {
+    $rootDir = dirname(__DIR__, 1);
+    exec("echo > $rootDir/logs/access.log");
+    exec("echo > $rootDir/logs/error.log");
+}
+
 Route::start();
