d1e98dcd2815d966522f653a40dcfefb2f7b7176
Aladser <Aladser@users.noreply.github.com>
2024-11-05 12:01:56 +0900

    обновлен boostrap

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -4,5 +4,8 @@ namespace App;
 
 use App\Core\Route;
 
-require dirname(__DIR__, 1).'/vendor/autoload.php';
+/**Корневая папка */
+define('ROOT_FOLDER', dirname(__DIR__, 1));
+
+require ROOT_FOLDER.'/vendor/autoload.php';
 Route::start();
